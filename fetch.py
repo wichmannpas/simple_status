1151dd5bbbe15c59af330cb96fa8b2edaa1e3cb4
Pascal Wichmann <wichmannpas@gmail.com>
2016-05-14 18:29:39 +0200

    Feature: Stats are collected

--- fetch.py
+++ fetch.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python3
+
+"""
+A simple status monitor.
+
+Generates a html page from a template and
+sends an email to the admin. Additionally it can collect stats about uptimes.
+"""
+
 import requests
 import re
 import datetime
 import os
+import json
 
 from jinja2 import Template
 
+check_interval = 5  # minutes
+keep_history = 365  # days
+stats_file = 'stats'
 hosts = [
-    ['http://www.profitopia.de/', 'Profitopia'],
-    ['http://help.profitopia.de/', 'Profitopia-Hilfe'],
-    ['http://blog.profitopia.de/', 'Profitopia-Blog'],
+    ['https://www.profitopia.de/', 'Profitopia'],
+    ['https://help.profitopia.de/', 'Profitopia-Hilfe'],
+    ['https://blog.profitopia.de/', 'Profitopia-Blog'],
 ]
-# mail command should contain recipient and have a single {} for subject formatting
+# mail command should contain recipient and have a single {} for subject format
 mail_command = '# {}'
 
 
@@ -26,7 +38,8 @@ def check_host(host):
     except Exception:
         host[1] = False
     if host[1] is False:
-        os.system(mail_command.format('CRITICAL: {} is critical'.format(host[0])))
+        os.system(mail_command.format('CRITICAL: {} is critical'.format(
+            host[0])))
 
     return host
 
@@ -40,15 +53,45 @@ def renew_status():
     return status
 
 
+def update_statistics(status):
+    """Add an entry to the stats file and delete too old entries."""
+    if not os.path.isfile(stats_file):
+        current_stats = {}
+    else:
+        current_stats = json.loads(open(stats_file, 'r').read())
+        current_stats = delete_old_statistics(current_stats)
+
+    current_key = datetime.datetime.now().strftime('%Y%m%d%H%M')
+    for host, up in status:
+        if host not in current_stats:
+            current_stats[host] = {}
+
+        # add current measurement
+        current_stats[host][current_key] = up
+
+    # write stats
+    open(stats_file, 'w').write(json.dumps(current_stats))
+
+
+def delete_old_statistics(stats):
+    """Delete too old statistics."""
+    # TODO
+    return stats
+
+
 def generate_status_page():
     """Generate a html page containing the status of the pages."""
     status = renew_status()
     now = datetime.datetime.now()
 
+    # update statistics
+    update_statistics(status)
+
     # load template
     template = Template(open('template.html').read())
     print(template.render({
-        'time': '{}.{}.{} {:02d}:{:02d}'.format(now.day, now.month, now.year, now.hour, now.minute),
+        'time': '{}.{}.{} {:02d}:{:02d}'.format(
+            now.day, now.month, now.year, now.hour, now.minute),
         'status': status,
     }))
 
